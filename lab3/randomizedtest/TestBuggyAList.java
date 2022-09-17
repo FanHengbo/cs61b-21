c466c720f141fddc88cf0eb363aa5eb3bd0c66f6
FanHengbo <fan_heng_bo@163.com>
2022-09-17 16:45:25 +0800

    fix the bug of removeLast

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,22 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void randomTest() {
+        BuggyAList<Integer> list1 = new BuggyAList<>();
+        final int N = 500;
+
+        for (int i = 0; i < N; ++i) {
+            int operation = StdRandom.uniform(0, 2);
+            if (operation == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                list1.addLast(randVal);
+                System.out.println("addLast("+randVal+")");
+            } else if (operation == 1) {
+                list1.removeLast();
+                System.out.println("removeLast()");
+            }
+        }
+    }
 }
